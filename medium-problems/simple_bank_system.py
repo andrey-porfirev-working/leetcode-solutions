0b5b376c514a6c336ef4ffa47e79ef5b58975620
Andrey Porfirev <andreyporfirevjob@gmail.com>
2025-10-26 12:05:53 +0300

    Add optimization for Simple Bank System problem - Runtime result: 30ms (66.87 % beaten) (was 36ms (50.62 % beaten)) - Memory result: 47.6 mb (96.00 % beaten) - Problem link: https://leetcode.com/problems/simple-bank-system/description/?envType=daily-question&envId=2025-10-26

--- medium-problems/simple_bank_system.py
+++ medium-problems/simple_bank_system.py
@@ -4,7 +4,7 @@ class Bank:
         self.balance = balance
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if 1 <= account1 <= len(self.balance) and 1 <= account2 <= len(self.balance):
+        if account1 <= len(self.balance) and account2 <= len(self.balance):
             if self.balance[account1 - 1] >= money:
                 self.balance[account1 - 1] -= money
                 self.balance[account2 - 1] += money
@@ -12,13 +12,13 @@ class Bank:
         return False
 
     def deposit(self, account: int, money: int) -> bool:
-        if 1 <= account <= len(self.balance):
+        if account <= len(self.balance):
             self.balance[account - 1] += money
             return True
         return False
 
     def withdraw(self, account: int, money: int) -> bool:
-        if 1 <= account <= len(self.balance):
+        if account <= len(self.balance):
             if self.balance[account - 1] >= money:
                 self.balance[account - 1] -= money
                 return True
