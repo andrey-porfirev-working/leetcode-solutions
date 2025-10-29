00e0d27d5a95aedd047d472d96fef299d7d3a047
Andrey Porfirev <andreyporfirevjob@gmail.com>
2025-10-30 00:28:08 +0300

    Add solution for Determine if Two Strings Are Closeproblem - Runtime: 83 ms (81.62 % beaten) - Memory: 18.35 mb (20.31 % beaten) - Link to a problem: https://leetcode.com/problems/determine-if-two-strings-are-close/?envType=study-plan-v2&envId=leetcode-75

---
+++ medium-problems/determine_if_two_strings_close.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        if set(word1) != set(word2):
+            return False
+        freq1 = sorted(Counter(word1).values())
+        freq2 = sorted(Counter(word2).values())
+        return freq1 == freq2
