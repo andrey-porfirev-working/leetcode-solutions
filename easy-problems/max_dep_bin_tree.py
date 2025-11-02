9e0b762987be901970224f698063e792f9fcee29
Andrey Porfirev <andreyporfirevjob@gmail.com>
2025-11-02 15:05:02 +0300

    Add solution for Maximum Depth of Binary Tree problem - Runtime result: 0ms (100 % beaten) - Memory result: 18.96 mb (78.67 % beaten) - Problem link: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75

--- easy-problems/max_dep_bin_tree.py
+++ easy-problems/max_dep_bin_tree.py
@@ -5,3 +5,20 @@ class Solution:
 
         return max(self.maxDepth(root.right), self.maxDepth(root.left)) + 1
 
+    def maxDepth_v_2(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, current_depth = stack.pop()
+            max_depth = max(max_depth, current_depth)
+
+            if node.right:
+                stack.append((node.right, current_depth + 1))
+            if node.left:
+                stack.append((node.left, current_depth + 1))
+
+        return max_depth
